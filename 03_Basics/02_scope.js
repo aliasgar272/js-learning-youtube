ebdca2f680f73e758e0ae5690bf23a3b1df1ef46
aliasgar272 <123356843+aliasgar272@users.noreply.github.com>
2025-02-06 04:59:23 +0000

    SCOPE COMPLETE

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -11,3 +11,41 @@ if(a < b){
 }
 console.log(a, b);
 
+
+// Scope 
+function one(){
+    const useName = "Ali"
+    function two (){
+        const myName = 'asgar'
+        console.log(useName);
+        
+    }
+    // console.log(myName);                        // My name will not be accessed since its outside the scope where it was declared
+    two()
+    
+}
+ one()
+/* ++++++++++++++++++++++ Interesting  ++++++++++++++++++++++   */
+
+// two ways of declaring a function 
+
+//first way which is normal way
+addOne(10)                                       // CAN BE CALLED BEFORE INITIALIZATION
+
+function addOne (num){
+    
+    return num + 1
+    
+}
+// Second way of declaring a function 
+// addTwo(10)                                  // CANNOT BE DELCARED BEFORE INITIALIZATION
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addOne(10));
+console.log(addTwo(10));
+console.log(typeof addOne);
+console.log( typeof addTwo);
+
+
